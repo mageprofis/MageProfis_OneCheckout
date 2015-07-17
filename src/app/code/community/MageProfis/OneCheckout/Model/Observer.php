7bd6d24b82a8025d3c4c0420583ac29be9dd195b
Mathis Klooß <mathis@mage-profis.de>
2015-07-17 17:25:23 +0200

    added newsletter support

--- src/app/code/community/MageProfis/OneCheckout/Model/Observer.php
+++ src/app/code/community/MageProfis/OneCheckout/Model/Observer.php
@@ -7,7 +7,8 @@
  * @copyright  Copyright (c) 2015 MageProfis GmbH
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class MageProfis_OneCheckout_Model_Observer extends Mage_Core_Model_Abstract
+class MageProfis_OneCheckout_Model_Observer
+extends Mage_Core_Model_Abstract
 {
     /**
      * 
@@ -62,5 +63,35 @@ class MageProfis_OneCheckout_Model_Observer extends Mage_Core_Model_Abstract
     public function setAddresses($observer)
     {
         Mage::helper("onecheckout")->setAddresses();
+    }
+
+    /**
+     * 
+     * @param Varien_Event_Observer $observer
+     */
+    public function addCommentToOrder($observer)
+    {
+        if($this->getRequest()->isPost())
+        {
+            $comment = $this->getRequest()->getPost('ordercomment', false);
+            if($comment && strlen(trim($comment)) > 0)
+            {
+                $order = $observer->getOrder();
+                /* @var Mage_Sale_Model_Order $order */
+                $comment = nl2br(Mage::helper('core')->escapeHtml(trim($comment)));
+                $order->setCustomerComment($comment);
+                $order->setCustomerNoteNotify(true);
+                $order->setCustomerNote($comment);
+            }
+        }
+    }
+
+    /**
+     * 
+     * @return Mage_Core_Controller_Request_Http
+     */
+    protected function getRequest()
+    {
+        return Mage::app()->getRequest();
     }
 }
