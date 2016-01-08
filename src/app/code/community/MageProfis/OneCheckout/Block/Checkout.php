186f639a3502401e83d39a6d7336944316abc550
Leon <leon@mage-profis.de>
2016-01-08 13:36:42 +0100

    Fix bug with ship to billing address; Don't set default values for city, region, post code und street.

--- src/app/code/community/MageProfis/OneCheckout/Block/Checkout.php
+++ src/app/code/community/MageProfis/OneCheckout/Block/Checkout.php
@@ -41,7 +41,9 @@ extends Mage_Core_Block_Template
         /* @var $shippingBlock Mage_Checkout_Block_Onepage_Shipping */
         if ($shippingBlock instanceof Mage_Checkout_Block_Onepage_Shipping)
         {
-            return $shippingBlock->getAddress()->getSameAsBilling();
+            $same_as_billing = $shippingBlock->getAddress()->getSameAsBilling();
+            
+            return $same_as_billing==null ? 1 : (int)$same_as_billing;
         }
         return false;
     }
